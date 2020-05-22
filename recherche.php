e08b52582f2cb0dd8324adf609371097498aa304
lauri <laurine.aujeau@isen-ouest.yncrea.fr>
2020-05-22 16:48:30 +0200

    v2

--- recherche.php
+++ recherche.php
@@ -26,23 +26,78 @@
     </div>
 </nav>
 <h1>Rechercher une citation</h1><hr>
+<form action="recherche.php" method="post">
 <h4>Auteur :</h4>
-<select class="custom-select">
-    <option selected>-- Auteur --</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
-</select>
-<br><br><h4>Siècle :</h4>
-<select class="custom-select">
-    <option selected>-- Siècle --</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
-</select>
-<br><br><button type="submit" class="btn btn-primary">Rechercher</button>
-<?php
 
+
+
+
+<?php
+include "connexpdo.php";
+
+$base='pgsql:dbname=citations;host=127.0.0.1;port=5432';
+$user = 'postgres';
+$password = 'isen2018';
+
+$idcon = connexpdo($base,$user,$password);
+
+$query8= "SELECT * from auteur";
+$result8 = $idcon->query($query8);
+echo '<select class="custom-select" name="auteur">';
+echo '<option >-- Auteur --</option>';
+foreach($result8 as $data2){
+    echo '<option>'.$data2['nom'].'</option>';
+
+}
+echo '</select>';
+
+echo '<br><br><h4>Siècle :</h4>';
+$query9= "SELECT * from siecle";
+$result9 = $idcon->query($query9);
+echo '<select class="custom-select" name="siecle">';
+echo '<option >-- Siècle --</option>';
+foreach($result9 as $data2){
+    echo '<option >'.$data2['numero'].'</option>';
+
+}
+echo '</select>';
+echo '<br><br><button type="submit" class="btn btn-primary">Rechercher</button><br><br>';
+
+
+if (isset($_POST['auteur']) && isset($_POST['siecle'])) {
+    $auteur = $_POST['auteur'];
+    $siecle = $_POST['siecle'];
+    echo '<table class="table">';
+    echo '<thead>';
+    echo '<tr>';
+    echo '<th scope="col"> Citation </th>';
+    echo '<th scope="col"> Auteur </th>';
+    echo '<th scope="col"> Siecle </th>';
+    echo '</tr>';
+    echo '</thead>';
+
+    $query = "SELECT id FROM siecle WHERE numero = '$siecle'";
+    $result = $idcon->query($query);
+    $siecleid = $result->fetch()['id'];
+    $query1 = "SELECT id FROM auteur WHERE nom = '$auteur'";
+    $result1 = $idcon->query($query1);
+    $auteurid = $result1->fetch()['id'];
+
+    $query2 = "SELECT phrase FROM citation WHERE auteurid = $auteurid and siecleid = $siecleid";
+    $result2 = $idcon->query($query2);
+    foreach ($result2 as $data) {
+        echo '<tr>';
+        echo '<td>' . $data ['phrase'] . '</td>';
+        echo '<td>' . $auteur . '</td>';
+        echo '<td>' . $siecle . '</td>';
+        echo '</tr>';
+    }
+
+    echo '</table>';
+
+}
 ?>
+
+</form>
 </body>
 </html>
